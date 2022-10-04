a2e9ad00a60d07f248087cf028bd613d07a827ec
EduardoViega <eduardoviega12@gmail.com>
2022-10-03 21:57:57 -0300

    DOM final

--- 02- DOM/desafio-02/app.js
+++ 02- DOM/desafio-02/app.js
@@ -2,5 +2,13 @@ new Vue({
     el: '#desafio',
     data: {
         valor: ''
+    },
+    methods: {
+        exibir: function() {
+            alert('aaaaAAAAAAAAAA!!!!!!!!!!!!!')
+        },
+        guardaData: function(event) {
+            this.valor = event.target.value
+        }
     }
 })
