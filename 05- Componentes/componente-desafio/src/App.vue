ba8e630c0c1edccda5d5729362fe3654d6fc7188
Eduardo Viega <eduardoviega12@gmail.com>
2022-10-05 17:45:33 -0300

    Desafio componentes

---
+++ 05- Componentes/componente-desafio/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <Menu/>
+    <Toolbar/>
+    <v-content>
+        <Carousel/>
+    </v-content>
+    <Footer/>
+  </v-app>
+</template>
+
+<script>
+import Footer from './components/template/Footer.vue'
+import Carousel from './components/widgets/Carousel.vue'
+import Toolbar from './components/template/Toolbar.vue'
+import Menu from './components/template/Menu.vue'
+
+export default {
+  components: {
+    Footer, Carousel, Toolbar, Menu,
+  }
+}
+</script>
