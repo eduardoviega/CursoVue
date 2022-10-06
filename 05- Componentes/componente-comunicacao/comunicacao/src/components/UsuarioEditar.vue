e9cc98a7f5045d318695707c0dff4753f970e3ca
EduardoViega <eduardoviega12@gmail.com>
2022-10-05 23:53:00 -0300

    Componente comunicação

---
+++ 05- Componentes/componente-comunicacao/comunicacao/src/components/UsuarioEditar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="componente">
+        <h3>Alterar os Dados de Usuário</h3>
+        <h6>Edite as informações</h6>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #98b99a;
+        color: #fff;
+    }
+</style>
---
+++ 05- Componentes/componente-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="componente">
+        <h3>As Informações de Usuário</h3>
+        <h6>Vários detalhes...</h6>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
