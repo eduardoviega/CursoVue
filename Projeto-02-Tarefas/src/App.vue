7ffbfb6d93124c92b50bbf624f54efc983b54261
Eduardo Viega <eduardoviega12@gmail.com>
2022-10-07 13:37:55 -0300

    Projeto 2 finalizado

--- Projeto-02-Tarefas/src/App.vue
+++ Projeto-02-Tarefas/src/App.vue
@@ -1,12 +1,64 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+		<TasksProgress :progress="progress" />
+		<NewTask @taskAdded="addTask"></NewTask>
+		<TaskGrid 
+			@taskDeleted="deleteTask"
+			@taskChanged="toogleTaskStage"
+			:tasks="tasks" />
 	</div>
 </template>
 
 <script>
+import TasksProgress from './components/TasksProgress.vue'
+import NewTask from './components/NewTask.vue'
+import TaskGrid from './components/TaskGrid.vue'
 export default {
-
+	components: { TaskGrid, NewTask, TasksProgress },
+	data() {
+		return {
+			tasks: []
+		}
+	},
+	computed: {
+		progress(){
+			const total = this.tasks.length 
+			const done = this.tasks.filter(t => !t.pending).length
+			return Math.round(done / total * 100) || 0
+		}
+	},
+	watch: {
+		tasks: {
+			deep: true,
+			handler(){
+				localStorage.setItem('tasks', JSON.stringify(this.tasks))
+			}
+		}
+	},
+	methods: {
+		addTask(task){
+			const sameName = t => t.name === task.name
+			const reallyNew = this.tasks.filter(sameName).length == 0
+			if (reallyNew && task.name != '') {
+				this.tasks.push({
+					name: task.name,
+					pending: task.pending || true
+				})
+			}
+		},
+		deleteTask(i){
+			this.tasks.splice(i, 1)
+		},
+		toogleTaskStage(i){
+			this.tasks[i].pending = !this.tasks[i].pending
+		}
+	},
+	created(){
+		const json = localStorage.getItem('tasks')
+		const array = JSON.parse(json)
+		this.tasks = Array.isArray(array) ? array : []
+	}
 }
 </script>
 
