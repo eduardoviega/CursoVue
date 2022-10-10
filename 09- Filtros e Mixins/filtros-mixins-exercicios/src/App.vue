3fa4b0a7a487cdda6e3840e8ac2b8cf1d258b251
Eduardo Viega <eduardoviega12@gmail.com>
2022-10-10 17:59:43 -0300

    Filtros e Mixins

---
+++ 09- Filtros e Mixins/filtros-mixins-exercicios/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+		<hr>
+		<p> {{ cpfDoAluno | cpf | inverter }}</p>
+		<input type="text" :value="cpfDoAluno | cpf | inverter" />
+		<hr>
+		<Frutas></Frutas>
+	</div>
+</template>
+
+<script>
+import Frutas from './Frutas.vue'
+export default {
+	components: {
+		Frutas
+	},
+	filters: {
+		cpf(valor){
+			const arr = valor.split('')
+			arr.splice(3, 0, '.')
+			arr.splice(7, 0, '.')
+			arr.splice(11, 0, '-')
+			return arr.join('')
+		}
+	},
+	data() {
+		return {
+			cpfDoAluno: '60070080090'
+		}
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+input {
+	font-size: 2.5rem;
+}
+ul {
+	list-style: none;
+}
+</style>
